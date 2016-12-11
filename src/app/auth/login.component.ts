c7bf28dd0bd4866adda451b71fbaa7bf7f5c197d
trinhtv060392 <trinhtv060392@gmail.com>
2016-12-11 17:32:09 +0700

    add form validation

--- src/app/auth/login.component.ts
+++ src/app/auth/login.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 
+import { User } from './user';
+
 @Component({
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 
 export class LoginComponent {
+
+	user: User = {
+	  email: '',
+	  password: 'Windstorm'
+	};
 }
